11ad82b3be81d9ed7fc80316f02c0ff5d4c93648
sromano1776 <romanos@udel.edu>
2020-03-09 20:01:26 -0400

    Update get_tweets.py

--- get_tweets.py
+++ get_tweets.py
@@ -7,6 +7,7 @@ def access_Twitter(handle):
         access_key=""
         access_secret=""
 
+
         api=tweepy.OAuthHandler(consumer_key, consumer_secret)
         api.set_access_token(access_key, access_secret)
 
@@ -15,15 +16,14 @@ def access_Twitter(handle):
         timeline=[]
 
         try:
-                timeline=api_access.user_timeline(screen_name=handle, count=200, tweet_mode="extended")
+                timeline=api_access.user_timeline(screen_name=handle[1:], count=200, tweet_mode="extended")
         except tweepy.TweepError as error:
                 print("An error has occurred: ", error)
                 return
-        filename=handle+'.txt'
+        filename=handle[1:]+'.txt'
         file=open(filename, 'w')
         for i in range (0, len(timeline)):
                 file.write(timeline[i].full_text.encode('utf-8').strip())
                 file.write('\n')
         file.close()
-
 access_Twitter(sys.argv[1])
